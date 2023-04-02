7b8c72c8d385bac5f0d7d171203165dc4f3f34cf
kritika20sharma <kritikasharma.17ee@saividya.ac.in>
2023-04-02 22:34:29 +0530

    Add project files.

---
+++ MatchGameRetried1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGameRetried1
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+       
+
+        private void SetUpGame()
+        {
+            List<string> animalemoji = new List<string>()
+            {
+            "🐵","🐵",
+            "🐺","🐺",
+            "🦁","🦁",
+            "🦌","🦌",
+            "🦣","🦣",
+            "🐧","🐧",
+            "🐍","🐍",
+            "🐬","🐬",
+            };
+
+            Random random = new Random();
+
+            foreach(TextBlock textblock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalemoji.Count);
+                string nextEmoji = animalemoji[index];
+                textblock.Text = nextEmoji;
+                animalemoji.RemoveAt(index);
+            }
+
+        }
+    }
+}
