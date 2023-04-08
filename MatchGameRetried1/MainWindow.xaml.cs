eab30ec136d7c5b7e11426dd93e93f791cd459d7
kritika20sharma <kritikasharma.17ee@saividya.ac.in>
2023-04-08 23:11:45 +0530

    A timer was added and actual game was made.

--- MatchGameRetried1/MainWindow.xaml.cs
+++ MatchGameRetried1/MainWindow.xaml.cs
@@ -12,19 +12,37 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MatchGameRetried1
 {
-    
+    using System.Windows.Threading;
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_tick;
+
             SetUpGame();
         }
 
-       
+        private void Timer_tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed/ 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - Play Again? ";
+            }
+        }
 
         private void SetUpGame()
         {
@@ -42,14 +60,58 @@ namespace MatchGameRetried1
 
             Random random = new Random();
 
-            foreach(TextBlock textblock in mainGrid.Children.OfType<TextBlock>())
+            foreach (TextBlock textblock in mainGrid.Children.OfType<TextBlock>())
             {
-                int index = random.Next(animalemoji.Count);
-                string nextEmoji = animalemoji[index];
-                textblock.Text = nextEmoji;
-                animalemoji.RemoveAt(index);
+                if (textblock.Name != "timeTextBlock")
+                {
+                    textblock.Visibility = Visibility.Visible;
+                    int index = random.Next(animalemoji.Count);
+                    string nextEmoji = animalemoji[index];
+                    textblock.Text = nextEmoji;
+                    animalemoji.RemoveAt(index);
+                }
             }
 
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+
+        }
+
+        /* the variables inside the class */
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+
+            if (findingMatch == false)             /* if nothing clicked */
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)  /* if 2nd click & last click matched*/
+            {
+                matchesFound++;                         /* count the matches found by player everytime */
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else        /* if 2nd click & last click does not match, which is why visibility is visible */
+            {
+                textBlock.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                SetUpGame();
+            }
         }
     }
 }
